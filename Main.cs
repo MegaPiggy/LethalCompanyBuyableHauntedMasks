0e6a3a823896df16c1c9083e45318a86199050e6
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-07-28 03:52:17 -0400

    Reverse

--- Main.cs
+++ Main.cs
@@ -30,7 +30,7 @@ namespace BuyableHauntedMasks
 
         private static ManualLogSource LoggerInstance => Instance.Logger;
 
-        public static List<Item> AllItems => Resources.FindObjectsOfTypeAll<Item>().Concat(UnityEngine.Object.FindObjectsByType<Item>(FindObjectsInactive.Include, FindObjectsSortMode.InstanceID)).ToList();
+        public static List<Item> AllItems => Resources.FindObjectsOfTypeAll<Item>().Reverse().ToList();
         public static Item Comedy => AllItems.FirstOrDefault(item => item.name.Equals("ComedyMask") && item.spawnPrefab != null);
         public static Item Tragedy => AllItems.FirstOrDefault(item => item.name.Equals("TragedyMask") && item.spawnPrefab != null);
         public static Item ComedyClone { get; private set; }
